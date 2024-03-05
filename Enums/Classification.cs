56f2df468f0417d51180d4518fd19f2f491d2618
Dani <daniela-park@outlook.com>
2024-03-05 12:29:45 +0000

    Initial commit

---
+++ Enums/Classification.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZooManagement.Enums;
+
+public enum Classification
+{
+    Mammal,
+    Reptile,
+    Bird,
+    Insect,
+    Fish,
+    Invertebrate
+}
